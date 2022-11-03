ebab9279d855c5c8c856a3842fc9b82dc02cf160
Fidan-Qurbanova <65399962+Fidan-Qurbanova@users.noreply.github.com>
2022-11-03 17:32:58 +0400

    Add files via upload

---
+++ Homework2.kt
@@ -0,0 +1,48 @@
+fun main() {
+    val result1 =sumOfAngles(10)
+     println("İç açılar toplamı $result1")
+    val result2 = findSalary(30)
+    println("Salary $result2")
+
+    val result3 = findPrice(60)
+    println("Price $result3")
+
+
+    val result4 = findFaranhiet(35)
+    println("FaranHiet $result4")
+}
+
+fun sumOfAngles(angleNum:Int): Int {
+    var  sumAngle = (angleNum - 2) * 180
+    return sumAngle
+}
+
+fun findSalary(workDay :Int):Int{
+    var result=0
+    var workHour = workDay*8;
+
+    if(workHour <160) return  workHour*10
+    else if (workHour >160)
+        return (workHour-160)*20 +1600
+    return  0
+}
+
+fun findPrice(limit :Int) :Int{
+    if(limit <50){ return limit * 2
+    }
+    else if (limit>50){
+        return 100 + ((limit-50)*4)
+
+    }
+    return 0
+}
+
+fun findFaranhiet(celcius : Int ): Double{
+return (celcius * 1.8) +32
+}
+
+
+
+
+
+
